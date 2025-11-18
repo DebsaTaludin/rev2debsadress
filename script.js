c3fb0983bc36dc1761cb8b02964e68fa91658a69
DebsaTaludin <debsagineral398@gmail.com>
2025-11-18 09:17:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const contacts = [
+    {
+        id: 1,
+        fullName: "Debsa Taludin",
+        phone: 6282149674730,
+        email: "debsagineral398@gmail.com",
+        location: "Sambas",
+    },
+    {
+        id: 2,
+        fullName: "Mochamad Irvan",
+        phone: 62881080070700,
+        email: "mchmdirvan@example.com",
+        location: "Jakarta",
+    },
+    {
+        id: 3,
+        fullName: "Adhitya Sofyan",
+        phone: 62881080080800,
+        email: "adhitya@example.com",
+        location: "Bandung",
+    },
+];
+
+console.log(contacts);
